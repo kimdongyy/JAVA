37eb86e53ac84fedca52d2acc62fb1d8f3505b29
kimdongyy <ehddud7107@gmail.com>
2022-01-01 18:48:33 +0900

    실행문 예시1

---
+++ src/chap02/RunStatementExample.java
@@ -0,0 +1,15 @@
+package chap02;
+
+public class RunStatementExample {
+
+	public static void main(String[] args) {
+		
+		int x = 1;
+		int z = 2;
+		int result = x + z;
+		
+		System.out.println(result);
+
+	}
+
+}
