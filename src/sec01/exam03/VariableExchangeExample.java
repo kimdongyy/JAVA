6e20e938b7193bb21f4043674b75eef12415a138
kimdongyy <ehddud7107@gmail.com>
2022-01-03 01:05:41 +0900

    Merge remote-tracking branch 'origin/master'

--- src/sec01/exam03/VariableExchangeExample.java
+++ src/sec01/exam03/VariableExchangeExample.java
@@ -3,8 +3,8 @@ package sec01.exam03;
 public class VariableExchangeExample {
 
 	public static void main(String[] args) {
-		int x = 35;
-		int y = 55;
+		int x = 3;
+		int y = 5;
 		System.out.println("x:" + x + "y:" + y );
 		
 		int temp = x;
