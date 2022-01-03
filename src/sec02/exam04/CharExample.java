2ec82b3733b7eca7b29ba3f9c47c01f15ec09461
kimdongyy <ehddud7107@gmail.com>
2022-01-03 21:31:19 +0900

    char type example

---
+++ src/sec02/exam04/CharExample.java
@@ -0,0 +1,18 @@
+package sec02.exam04;
+
+public class CharExample {
+
+	public static void main(String[] args) {
+
+		char c1 = 'A';
+		char c2 = 888;
+		char c3 = '\u0041';
+		char c4 = '가';
+		//유니코드 번호를 저장해라
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c3);
+		System.out.println(c4);
+	}
+
+}
