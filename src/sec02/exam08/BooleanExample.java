a5bc0f36648a472899950bf9f4aae73ebc31478a
kimdongyy <ehddud7107@gmail.com>
2022-01-03 23:43:08 +0900

    boolean example

---
+++ src/sec02/exam08/BooleanExample.java
@@ -0,0 +1,15 @@
+package sec02.exam08;
+
+public class BooleanExample {
+
+	public static void main(String[] args) {
+		boolean stop = false;
+		
+		if(stop) {
+			System.out.println("∏ÿ√Á");
+	} else {
+			System.out.println("∞Ëº”∞°");
+	}
+	}
+
+}
