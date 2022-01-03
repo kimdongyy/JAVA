50012747e5666b8be6d610f8421cbb333e43f2de
kimdongyy <ehddud7107@gmail.com>
2022-01-03 23:39:03 +0900

    Float, double example

---
+++ src/sec02/exam07/FloatDoubleExample.java
@@ -0,0 +1,28 @@
+package sec02.exam07;
+
+public class FloatDoubleExample {
+
+	public static void main(String[] args) {
+
+			Float var1 = 3.14f;
+			Double var2 = 3.14;
+			System.out.println(var1);
+			System.out.println(var2);
+			
+			Float var3 = 0.222222222222222f;
+			double var4 = 0.2222222222222222;
+			
+			
+			System.out.println(var3);
+			System.out.println(var4);
+			
+			double var5 = 3e6;
+			float var6 = 3e6f;
+			double var7 = 2e-3;
+			
+			System.out.println(var5);
+			System.out.println(var6);
+			System.out.println(var7);
+	}
+
+}
