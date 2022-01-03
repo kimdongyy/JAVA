0d38ca0f6831883469200968b1d5d97e26829bef
kimdongyy <ehddud7107@gmail.com>
2022-01-03 21:18:10 +0900

    byte 출력 예시

---
+++ src/sec02/exam02/byteExample.java
@@ -0,0 +1,15 @@
+package sec02.exam02;
+
+public class byteExample {
+
+	public static void main(String[] args) {
+		byte var1 = -128;
+		byte var2 = 127;
+		byte var3 = 0;
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+	}
+
+}
