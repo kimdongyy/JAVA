55692c397fbc9c1e61dc08fd3212a2f812b97a2c
kimdongyy <ehddud7107@gmail.com>
2022-01-03 21:55:17 +0900

    string 예시

---
+++ src/sec02/exam05/StringExample.java
@@ -0,0 +1,14 @@
+package sec02.exam05;
+
+public class StringExample {
+
+	public static void main(String[] args) {
+		String name = "È«±æµ¿";
+		String job = "µµµÏ";
+				
+		System.out.println(name);
+		System.out.println(job);
+
+	}
+
+}
